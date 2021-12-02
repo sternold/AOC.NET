be79a0b8d0b9a61043916bbbc7fefad270e9eae3
Tobi van Bronswijk <tvanbronswijk@gmail.com>
2021-12-02 07:46:33 +0100

    Advent of Code 2021 - Day 2

---
+++ AOC21/Day02.cs
@@ -0,0 +1,53 @@
+﻿namespace AOC21
+{
+    public class Day02 : BaseDay
+    {
+        public IEnumerable<(string Direction, int Distance)> Input { get; }
+
+        public Day02()
+        {
+            Input = FileReader.ReadAllLines(InputFilePath).Select(l => l.Split(' ')).Select(arr => (arr[0], int.Parse(arr[1])));
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            return new(Input.Aggregate<(string Direction, int Distance), (int H, int D), int>((0, 0), (agg, comm) =>
+            {
+                switch (comm.Direction)
+                {
+                    case "forward":
+                        agg.H += comm.Distance;
+                        break;
+                    case "up":
+                        agg.D -= comm.Distance;
+                        break;
+                    case "down":
+                        agg.D += comm.Distance;
+                        break;
+                }
+                return agg;
+            }, (agg) => agg.H * agg.D).ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            return new(Input.Aggregate<(string Direction, int Distance), (int H, int D, int A), int>((0, 0, 0), (agg, comm) =>
+            {
+                switch (comm.Direction)
+                {
+                    case "forward":
+                        agg.H += comm.Distance;
+                        agg.D += agg.A * comm.Distance;
+                        break;
+                    case "up":
+                        agg.A -= comm.Distance;
+                        break;
+                    case "down":
+                        agg.A += comm.Distance;
+                        break;
+                }
+                return agg;
+            }, (agg) => agg.H * agg.D).ToString());
+        }
+    }
+}
