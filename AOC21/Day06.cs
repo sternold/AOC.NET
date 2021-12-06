cd37ac0122e77416efed51c8af409c805d4d44cb
Tobi van Bronswijk <tvanbronswijk@gmail.com>
2021-12-06 10:23:12 +0100

    Advent of Code 2021 - Day 6

---
+++ AOC21/Day06.cs
@@ -0,0 +1,53 @@
+﻿namespace AOC21
+{
+    public class Day06 : BaseDay
+    {
+        public IEnumerable<int> Input { get; }
+
+        public Day06()
+        {
+            Input = FileReader.ReadAllLines(InputFilePath).First().Split(',').Select(num => int.Parse(num));
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            return new(GenerateLanternfish(Input, 80).Sum(kvp => kvp.Value).ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            return new(GenerateLanternfish(Input, 256).Sum(kvp => kvp.Value).ToString());
+        }
+
+        public static IDictionary<int, long> GenerateLanternfish(IEnumerable<int> seed, int size)
+        {
+            var lanternfish = seed.ToLookup(num => num).ToDictionary(grp => grp.Key, grp => grp.LongCount());
+            for (var i = 0; i < size; i++)
+            {
+                var births = 0L;
+                for (var j = 0; j < 9; j++)
+                {
+                    if (!lanternfish.ContainsKey(j))
+                    {
+                        continue;
+                    }
+                    if (j == 0)
+                    {
+                        births = lanternfish[j];
+                    }
+                    else
+                    {
+                        lanternfish.Add(j - 1, lanternfish[j]);
+                    }
+                    lanternfish.Remove(j);
+                }
+                if (!lanternfish.TryAdd(6, births))
+                {
+                    lanternfish[6] += births;
+                }
+                lanternfish.Add(8, births);
+            }
+            return lanternfish;
+        }
+    }
+}
