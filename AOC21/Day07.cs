b60ed143209521cba4b7000ef424a30b1d4618da
Tobi van Bronswijk <tvanbronswijk@gmail.com>
2021-12-07 08:22:10 +0100

    Advent of Code 2021 - Day 7

---
+++ AOC21/Day07.cs
@@ -0,0 +1,40 @@
+﻿namespace AOC21
+{
+    public class Day07 : BaseDay
+    {
+        public IEnumerable<int> Input { get; }
+
+        public Day07()
+        {
+            Input = FileReader.ReadAllLines(InputFilePath).First().Split(',').Select(num => int.Parse(num));
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            var result = int.MaxValue;
+            for(int i = Input.Min(); i <= Input.Max(); i++)
+            {
+                var diff = Input.Select(num => Math.Abs(num - i)).Sum();
+                if(diff < result)
+                {
+                    result = diff;
+                }
+            }
+            return new(result.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            var result = int.MaxValue;
+            for (int i = Input.Min(); i <= Input.Max(); i++)
+            {
+                var diff = Input.Select(num => Math.Abs(num - i)).Select(dis => dis*(dis+1)/2).Sum();
+                if (diff < result)
+                {
+                    result = diff;
+                }
+            }
+            return new(result.ToString());
+        }
+    }
+}
