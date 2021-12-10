1635fe6d1af935e83ad1fdb25c0422d061f37677
Tobi van Bronswijk <tvanbronswijk@gmail.com>
2021-12-10 16:27:05 +0100

    Advent of Code 2021 - Day 10

---
+++ AOC21/Day10.cs
@@ -0,0 +1,66 @@
+﻿namespace AOC21
+{
+    public class Day10 : BaseDay
+    {
+        public static readonly Dictionary<char, char> Pairs = new()
+        {
+            { '<', '>' },
+            { '[', ']' },
+            { '{', '}' },
+            { '(', ')' }
+        };
+
+        public IEnumerable<string> Input { get; }
+
+        public Day10()
+        {
+            Input = FileReader.ReadAllLines(InputFilePath);
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            return new(FindCorruptSymbols(Input)
+                       .Where(line => line.Corrupt.HasValue)
+                       .Sum(line => line.Corrupt switch
+                       {
+                           ')' => 3,
+                           ']' => 57,
+                           '}' => 1197,
+                           '>' => 25137,
+                           _ => 0
+                       })
+                       .ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            return new(FindCorruptSymbols(Input)
+                       .Where(line => !line.Corrupt.HasValue)
+                       .Select(line => line.Stack.Select(c => Pairs[c])
+                                                 .Aggregate(0L,
+                                                 (agg, next) => (agg * 5) + (next switch
+                                                 {
+                                                     ')' => 1L,
+                                                     ']' => 2L,
+                                                     '}' => 3L,
+                                                     '>' => 4L,
+                                                     _ => 0L
+                                                 })))
+                       .Median()
+                       .ToString());
+        }
+
+        public static IEnumerable<(char? Corrupt, Stack<char> Stack)> FindCorruptSymbols(IEnumerable<string> lines)
+            => lines.Select(line => FindCorruptSymbol(line));
+
+        public static (char? Corrupt, Stack<char> Stack) FindCorruptSymbol(string line)
+            => line.Aggregate((Result: new char?(), Stack: new Stack<char>()),
+                (agg, next) =>
+                {
+                    if (agg.Result != null) return agg;
+                    else if (Pairs.ContainsKey(next)) agg.Stack.Push(next);
+                    else if (next != Pairs[agg.Stack.Pop()]) agg.Result = next;
+                    return agg;
+                });
+    }
+}
---
+++ AOC21/Shared/LinqExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace System.Linq
+{
+    public static class LinqExtensions
+    {
+        public static double Median(this IEnumerable<long> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            var data = source.OrderBy(n => n).ToArray();
+            if (data.Length == 0)
+                throw new InvalidOperationException();
+            if (data.Length % 2 == 0)
+                return (data[data.Length / 2 - 1] + data[data.Length / 2]) / 2.0;
+            return data[data.Length / 2];
+        }
+    }
+}
