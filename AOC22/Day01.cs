55c25b2f2d0a5f50fd14c9a774e55544e7b32235
Tobi van Bronswijk <dev@tvbronswijk.nl>
2022-12-02 12:55:59 +0100

    Advent of Code 2022 - Day 1

---
+++ AOC22/Day01.cs
@@ -0,0 +1,30 @@
+﻿using AOC22.Shared;
+
+namespace AOC22
+{
+    internal class Day01 : BaseDay
+    {
+
+        private readonly string _input;
+
+        public Day01()
+        {
+            _input = FileReader.ReadAllText(InputFilePath);
+        }
+
+        public override ValueTask<string> Solve_1() => new(_input
+            .Split((Environment.NewLine + Environment.NewLine), StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => s.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+            .Max(arr => arr.Sum(s => int.Parse(s)))
+            .ToString());
+
+        public override ValueTask<string> Solve_2() => new(_input
+            .Split((Environment.NewLine + Environment.NewLine), StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => s.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+            .Select(arr => arr.Sum(s => int.Parse(s)))
+            .OrderByDescending(v => v)
+            .Take(3)
+            .Sum()
+            .ToString());
+    }
+}
---
+++ AOC22/Program.cs
@@ -0,0 +1,16 @@
+﻿global using AoCHelper;
+
+if (args.Length == 0)
+{
+    Solver.SolveLast();
+}
+else if (args.Length == 1 && args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase))
+{
+    Solver.SolveAll();
+}
+else
+{
+    Solver.Solve(args.Select(arg => (Parsed: uint.TryParse(arg, out var value), Value: value))
+                     .Where(arg => arg.Parsed)
+                     .Select(arg => arg.Value));
+}
